a99267565b498a3afeddce4635d9bf180d5ef9ae
Masoud Darvishian <masoudarvishian@gmail.com>
2022-01-28 15:24:35 +0330

    Rename some stuff

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to codemasters-leaderboards-client!');
+    expect(page.getTitleText()).toEqual('Welcome to game-leaderboards-client!');
   });
 
   afterEach(async () => {
